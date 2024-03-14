5c0e523a16a8cb68bd2afcaced4e678c272ff779
Ruslan Poluyanov <monsterl33t@gmail.com>
2024-03-14 13:54:34 +0300

    fix

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const MONGO_DB_URL = 'mongodb://localhost:27017/bitfilmsdb';
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const JWT_SECRET_KEY = 'dev-secret-key';
 const rateLimit = require('express-rate-limit');
 
